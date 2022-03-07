bc6c520f15ea7795f3736a461952ccb862047d90
Lzhenghong <hong2288@gmail.com>
2022-03-07 21:23:25 +0800

    added prelim test header

--- scheduler-app/src/App.vue
+++ scheduler-app/src/App.vue
@@ -1,15 +1,18 @@
 <template>
+  <Header/>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to our Scheduler App Hope we get A"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
 }
 </script>
@@ -21,6 +24,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
