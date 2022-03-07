f601c8d1f20822977a8899edcbb8f845b5f7154c
Edward Loke <awkwardlook@gmail.com>
2022-03-07 21:09:25 +0800

    Testing change to main app

--- scheduler-app/src/App.vue
+++ scheduler-app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to our Scheduler App Hope we get A"/>
 </template>
 
 <script>
