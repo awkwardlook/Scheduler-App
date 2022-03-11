22b4bec869db70c49aa34ab08621bb7ea33135bc
Lzhenghong <hong2288@gmail.com>
2022-03-12 06:30:24 +0800

    form validation to avoid submission of empty username

--- scheduler-app/src/components/LoginRegister/Loginbox.vue
+++ scheduler-app/src/components/LoginRegister/Loginbox.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="loginbox">
+        <div v-if = "usernameEntered" class="backbuttondiv">
+            <button @click="editUsername()">Back</button>
+        </div>
         <form>
             <div class="loginformheader">
                 <h1>Log in to your Account</h1>
@@ -9,13 +12,12 @@
             <text id="loginformdesc">For both employees and employers, just add your username and corresponding password to log in.</text>
             <br><br>
 
-            <div class="loginforminput">
-                <input type="text" v-model = "username" id="username" placeholder="Username"> 
-            </div>
-            <br><br>
-
-            <div>
-                <button class = "usernamebutton" type="button" @click="setUsername()">Next</button> 
+            <div v-if = "!usernameEntered">
+                <div class="loginforminput">
+                    <input type="text" v-model = "username" id="username" placeholder="Username" required> 
+                </div>
+                <br><br>
+                <button class = "usernamebutton" type="button" @click="enterUsername()">Next</button> 
             </div>
         </form>
     </div>
@@ -27,12 +29,21 @@
 export default {
     data(){
         return{
-            username: ""
+            username: "",
+            usernameEntered: false
         }
     },
     methods: {
-        setUsername() {
-            alert("Username entered: " + this.username);
+        enterUsername() {
+            if (this.username != "") {
+                alert("Username entered: " + this.username);
+                this.usernameEntered = !this.usernameEntered;
+            } else {
+                alert("Please ensure username is filled in!");
+            }
+        },
+        editUsername() {
+            this.usernameEntered = !this.usernameEntered;
         }
     }
 }
@@ -48,21 +59,21 @@ export default {
         margin: 20 0 0 450px;  
         padding: 80px;  
         background: #ffffff;  
-        border-radius: 15px ;
+        border-radius: 15px;
     }
-
+    .backbuttondiv{
+        background-color: red;
+    }
     .loginformheader{
         height: 50px;
         text-align: center;
     }
-
     #loginformdesc{
         height: 50px;
         background-color: none;
         color: rgb(68, 68, 68);
         text-align: center;
     } 
-
     .usernamebutton {
         background-color: #0069e0; 
         border-radius: 30px;
@@ -74,7 +85,6 @@ export default {
         font-size: 14px;
         width: 120px;
     }
-
     .loginforminput{
         display: flex;
         flex-direction: row;
