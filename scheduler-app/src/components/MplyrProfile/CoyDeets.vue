306b56708a2cfa84c5c5904ccd764bae94958226
Edward Loke <awkwardlook@gmail.com>
2022-03-21 23:42:20 +0800

    BackEnd Profile
    FINALLY DID BACKEND !!!! Connected to firestore

--- scheduler-app/src/components/MplyrProfile/CoyDeets.vue
+++ scheduler-app/src/components/MplyrProfile/CoyDeets.vue
@@ -1,14 +1,14 @@
 <template>
     <div class= "coydeets">
         <h1>Company Details</h1>
-        <div class="header">
-            <h2>Company Name</h2>
-            <h5>-Company Logo-</h5>
+        <div class="left">
+            <h2 id = "coyname">Company Name</h2>
+            <h5 id = "coylogo">-Company Logo-</h5>
         </div>
 
-        <div class="description">
-            <h1>Company Description</h1>
-            <h2 id="coydesc"> Not Coy Desc</h2>            
+        <div class="right">
+            <h2>Company Description</h2>
+            <h5 id="coydesc">Wanted company description</h5>            
         </div>
     </div>
 
@@ -20,56 +20,38 @@ import firebase from 'firebase'
 // import { doc, getDoc } from "firebase/firestore";
 
 const db = firebase.firestore()
-const shopee = db.collection("companies").doc("shopee")
-console.log(shopee)
+const companies = db.collection("companies")
+const shopee = companies.doc("shopee")
 
-
+// console.log(shopee)
 
 export default {
     mounted(){
 
-    // const coyList = document.querySelector('#coy-list');
+    shopee.onSnapshot(function(doc) {
+        let data = doc.data();
+        // gets the value of a field called field1 from the doc
+        console.log(data)
+        const cname = data.name
+        const desc = data.description
+        const clogo = data.clogo
+        document.getElementById("coyname").innerText = cname
+        document.getElementById("coydesc").innerText = desc
+        document.getElementById("coylogo").innerText = clogo
+        
 
-    // // create element & companies
-    // function renderCafe(doc){
-    //     let li = document.createElement('li');
-    //     let name = document.createElement('span');
-    //     let city = document.createElement('span');
+        // let value1 = data.description;
 
-    //     li.setAttribute('data-id', doc.id);
-    //     name.textContent = doc.data().name;
-    //     city.textContent = doc.data().city;
-
-    //     li.appendChild(name);
-    //     li.appendChild(city);
-
-    //     coyList.appendChild(li);
+    })
+    //     
     // }
-
-    // // getting data
-    // db.collection('companies').get().then(snapshot => {
-    //     snapshot.docs.forEach(doc => {
-    //         renderCafe(doc);
-    //     });
-    // });
-    async function display(){
-
-        // const docSnap = await getDoc(cdescription)
-        db.getElementById("coydesc").innerHTML = (shopee.get("description"))
-    }
-    display()
+    // display()
     }
 }
 
 </script>
 
 <style scoped>
-    .header{
-        height: 50px;
-        background-color:  #ffffff;
-        text-align: center;
-    }
-
     .coydeets{
         text-align: center;
         font-size: 24px;
@@ -77,7 +59,7 @@ export default {
         overflow: hidden;  
     }
 
-    .header{
+    .left{
         float: left;
         width: 500;
         margin-left: 10px;
@@ -86,10 +68,10 @@ export default {
         /* border: 2px solid blue; */
     }
 
-    .description{
+    .right{
         float: right;
         width: 500px;
-        margin-left: 10px;
+        margin-right: 10px;
         margin-top: 10px;
         padding: 5px;
         /* border: 2px solid blue; */
--- scheduler-app/src/components/MplyrProfile/MplyrDeets.vue
+++ scheduler-app/src/components/MplyrProfile/MplyrDeets.vue
@@ -2,52 +2,60 @@
     
     <div class = "mdeets">
         <h1>Employer Details</h1>
-        <div class="header">
+        <h2 id="ename">Employer's Name</h2>
+        <div class="eleft">
             <br/><br/>
-            <h2>Employer Department</h2>
+            <h2 id = "edept">Employer Department</h2>
             <br/>
-            <h5>-Employer Photo-</h5>
+            <h5 id = "dp">-Employer Photo-</h5>
         </div>
 
 
-        <div class="description">
-            <h2 id="desc">Employer's Name</h2>
-                <h2> Gender</h2>
-                <table id = "tGender" class = "auto=index">
-                    <tr>
-                        <th>Gender</th>
-                    </tr>
-                </table>
-                <br>
-
-            <h2>Phone Number</h2>
-            <table id = "tPhone" class = "auto=index">
-                <tr> 
-                    <th>Phone Number</th>
-                </tr>
-            </table>
-            <br>
-
+        <div class="eright">
             <h2>Email</h2>
-            <table id = "tEmail" class = "auto=index">
-                    <tr>
-                        <th>Email</th>
-                    </tr>
-            </table>
+            <h3 id="email"> Email </h3>
+            <h2>Phone Number</h2>
+            <h3 id = "pnum">Phone Number</h3>
+            <h2> Gender </h2>
+            <h3 id = "gender">Gender</h3>
         </div>
     </div>
-
 </template>
 
 <script>
 // import firebaseApp from '../firebase.js';
 // import { getFirestore } from "firebase/firestore"
 // import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
+import firebase from 'firebase'
 
-// const db = getFirestore(firebaseApp);
+const db = firebase.firestore();
+const employers = db.collection("employers");
+const email = "employer1@gmail.com" ;
+const edward = employers.doc(email);
 
 export default {
-    mounted(){}
+    mounted(){
+        edward.onSnapshot(function(doc) {
+        let data = doc.data();
+        // gets the value of a field called field1 from the doc
+        // console.log(data)
+        
+        const ename = data.name
+        const edept = data.department
+        const dp = data.ephoto
+        const email = data.email
+        const pnum = data.pnum
+        const gender = data.gender
+        // const username = data.username
+
+        document.getElementById("ename").innerText = ename
+        document.getElementById("edept").innerText = edept
+        document.getElementById("dp").innerText = dp
+        document.getElementById("email").innerText = email
+        document.getElementById("pnum").innerText = pnum
+        document.getElementById("gender").innerText = gender
+        })
+    }
 }
 </script>
 
@@ -64,7 +72,7 @@ export default {
         width: 1000px;  
     }
 
-    .header{
+    .eleft{
         float: left;
         width: 400;
         margin-left: 10px;
@@ -73,7 +81,7 @@ export default {
         /* border: 2px solid blue; */
     }
 
-    .description{
+    .eright{
         float: right;
         width: 400px;
         margin-left: 10px;
