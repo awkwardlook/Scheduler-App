58cd15d1c79d625b42734346a62e603bd7d638b4
ohjiashang <77714248+ohjiashang@users.noreply.github.com>
2022-03-21 23:43:23 +0800

    required pages and components for availability form

---
+++ scheduler-app/src/components/MainSchedule/AvailForm.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>AvailForm</h1>
+</template>
+
+<script>
+export default {
+	name: 'AvailForm'
+}
+</script>
+
+<style>
+
+</style>
---
+++ scheduler-app/src/views/MainSchedule.vue
@@ -0,0 +1,20 @@
+<template>
+<h1>Main Schedule</h1>
+<AvailForm/>
+</template>
+
+<script>
+import AvailForm from '../components/MainSchedule/AvailForm.vue'
+
+export default {
+	name: 'MainSchedule',
+	components: {
+		AvailForm	
+	}
+
+}
+</script>
+
+<style>
+
+</style>
