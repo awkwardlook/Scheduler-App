d633781ef0f0633b1854e69578639e4638ccc7cd
vjo-anne209 <77625659+vjo-anne209@users.noreply.github.com>
2022-03-23 20:29:47 +0800

    Modified Landing Card

--- scheduler-app/src/components/LandingPage/LandingCard.vue
+++ scheduler-app/src/components/LandingPage/LandingCard.vue
@@ -1,7 +1,10 @@
 <template>
    <div class='hero-container'>
-        <img src="@/assets/scheduler_logo2.png" alt="logo" height= '180px' width='180px'>
         <h1 id='welcome'>Welcome to Scheduler</h1>
+        <div>
+          <img src="@/assets/scheduler_logo2.png" alt="logo" width='180px'>
+        </div>
+        <p>Welcome</p>
         <p>Plan your schedule</p>
         <p>Plan their schedule</p>
         <div class='hero-btns'>
