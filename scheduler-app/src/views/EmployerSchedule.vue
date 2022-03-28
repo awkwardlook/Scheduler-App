a7b95c1982368cac8c0ceafd0ade18bfcbcc8802
vjo-anne209 <77625659+vjo-anne209@users.noreply.github.com>
2022-03-29 00:48:24 +0800

    Added availability to employer schedule

--- scheduler-app/src/views/EmployerSchedule.vue
+++ scheduler-app/src/views/EmployerSchedule.vue
@@ -2,6 +2,10 @@
     <EmployerHeader/>
     <br><br>
     <h1>Employer Schedule Page</h1>
+    <div id='calendar' style="width: 80%; height:100%; display: inline-block;padding:15px; padding-bottom:20px">
+        <Availability/>
+    </div> 
+    
 </template>
 
 <style>
@@ -9,11 +13,12 @@
 
 <script>
 import EmployerHeader from '../components/Header/EmployerHeader.vue'
-
+import Availability from '../components/EmployeeSchedule/Availability.vue'
 export default {
     name: 'Employer Schedule',
     components: {
-        EmployerHeader
+        EmployerHeader,
+        Availability,
     }
 }
 </script>
