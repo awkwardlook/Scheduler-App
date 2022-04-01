3f11289bb23fa66570cf55e4362b978ca75cac43
ohjiashang <77714248+ohjiashang@users.noreply.github.com>
2022-04-01 21:56:52 +0800

    Linked pie chart & line chart to backend

--- scheduler-app/src/components/Chart.vue
+++ scheduler-app/src/components/Chart.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="line">
         <h2>Current Week's Work Strength</h2>
-        <button class="button" @click="updateBar()">Update</button><br><br><br>
-        <line-chart class = 'user' width =500px :data = "barchartdata"></line-chart >
+        <button class="button" @click="updateLineChart()">Update</button><br><br><br>
+        <line-chart class = 'user' width =500px :data = "linechartdata"></line-chart >
     </div><br><br>
 
     <div class="pie">
         <h2>Work Strength</h2>
-        <button class="button" @click="updatePie()">Update</button><br><br><br>
+        <button class="button" @click="updatePieChart()">Update</button><br><br><br>
         <pie-chart width =500px :data= "piechartdata"></pie-chart>
     </div>
 </template>
@@ -25,15 +25,16 @@ export default {
     data(){
 
         return{
-            barchartdata: {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0, "Friday": 0},
+            linechartdata: {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0, "Friday": 0},
             piechartdata: {"Morning": 0, "Afternoon": 0},
             selected:""
         }
     },
     
     methods:{
-        updatePie() {
+        updatePieChart() {
             var data = {"Morning": 0, "Afternoon": 0};
+            
             const shifts = db.collection("availabilities")
 
             shifts.get().then((querySnapshot) => {
@@ -51,16 +52,35 @@ export default {
                             data["Afternoon"] += 1
                         }
                     }
-
-                    console.log(data)
-                    this.piechartdata = data
                 })
-                
+                console.log(data)
+                this.piechartdata = data
             }) 
         },
 
-        updateBar: function (){
-            this.barchartdata = {'Monday': Math.random()*5, 'Tuesday': 5, 'Wednesday': Math.random()* 5, 'Thursday': 5, 'Friday':6}
+        updateLineChart() {
+            var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+            var data = {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0, "Friday": 0};
+            
+            const shifts = db.collection("availabilities")
+
+            shifts.get().then((querySnapshot) => {
+                const temp = []
+                querySnapshot.forEach((doc) => {
+                    temp.push({ id: doc.id, ...doc.data() })
+
+                    if (doc.data().approved) {
+                        const date = doc.id.slice(0,10)
+                        const day = new Date(date).getDay()
+                        const dayOfWeek = days[day]
+
+                        data[dayOfWeek] += 1
+                    }
+                })
+
+                console.log(data)
+                this.linechartdata = data  
+            }) 
         },
 
     },
