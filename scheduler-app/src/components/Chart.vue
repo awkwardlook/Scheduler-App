aa7e1319db2e883e9161d42a0fd01cff6772a228
Zhiqian <65587145+singingseaweed@users.noreply.github.com>
2022-04-01 18:18:40 +0800

    importing data into chart (not debugged)

--- scheduler-app/src/components/Chart.vue
+++ scheduler-app/src/components/Chart.vue
@@ -1,35 +1,65 @@
 <template>
     <div class="line">
         <h2>Current Week's Work Strength</h2>
-        <button class="button" @click="updateMe()">Update</button><br><br><br>
-        <line-chart class = 'user' width =500px :data = "chartdata"></line-chart >
+        <button class="button" @click="updateMe2()">Update</button><br><br><br>
+        <line-chart class = 'user' width =500px :data = "chartdata2"></line-chart >
     </div><br><br>
 
-    <div class="bar">
-        <h2>Overall Weekly Work Strength</h2>
-        <button class="button" @click="updateMe2()">Update</button><br><br><br>
-        <column-chart class ="user" width=500px :data="chartdata2"></column-chart>
+    <div class="pie">
+        <h2>Work Strength</h2>
+        <button class="button" @click="setup()">Update</button><br><br><br>
+        <pie-chart width =500px :data= "chartdata"></pie-chart>
     </div>
 </template>
 
 <script>
- export default {
+import firebase from 'firebase'
+
+const db = firebase.firestore()
+const shifts = db.collection("availabilities")
+
+export default {
+    //import firebase dated data 
+    //collect dates based on if statements into days of the week/collect by months
+    
     name:'Charts',
     data(){
+
         return{
-            chartdata: {'Monday': 2, 'Tuesday': 5, 'Wednesday': 2, 'Thursday': 5, 'Friday':6},
+            
             chartdata2: {'Blueberry':44, 'Strawberry':23},
+
+            chartdata: {},
             
             selected:""
         }
     },
     methods:{
+
+        setup(){
+            var data = {"morning": 0, "afternoon": 0};
+            shifts.get().then((docSnapshot) => {
+                if (docSnapshot.exists) {
+                    const dates = docSnapshot.data();
+                    if (dates.start.slice(11, 19) == "09:00:00") {
+                        data["morning"] = data["morning"] + 1
+                    }
+                    if (dates.start.slice(11, 19) == "15:00:00") {
+                        data["afternoon"] = data["afternoon"] + 1
+                    }
+                }
+            })
+            console.log("data")
+            return data;   
+        },
+
         updateMe: function (){
             this.chartdata = {'Monday': Math.random()*5, 'Tuesday': 5, 'Wednesday': Math.random()* 5, 'Thursday': 5, 'Friday':6}
         },
         updateMe2: function (){
             this.chartdata2 = {'Blueberry':Math.random()*30, 'Strawberry':23,'Balckberry':23}
-        }
+        },
+
     },
 }
 </script>
