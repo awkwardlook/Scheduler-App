ba981783bae7480df8a77a966bc13f92358983bd
Zhiqian <65587145+singingseaweed@users.noreply.github.com>
2022-04-05 12:07:46 +0800

    implementing dummy line chart for new chart

--- scheduler-app/src/components/Chart.vue
+++ scheduler-app/src/components/Chart.vue
@@ -1,14 +1,20 @@
 <template>
     <div class="line">
-        <h2>Current Week's Work Strength</h2>
+        <h2>Daily Work Strength</h2>
         <button class="button" @click="updateLineChart()">Update</button><br><br><br>
         <line-chart class = 'user' width =500px :data = "linechartdata"></line-chart >
     </div><br><br>
 
+    <div class="line2">
+        <h2>Current Week's Work Strength</h2>
+        <button class="button" @click="updateLineChart2()">Update</button><br><br><br>
+        <line-chart class = 'user' width =500px :data = "linechartdata2"></line-chart >
+    </div><br><br>
+
     <div class="pie">
-        <h2>Work Strength</h2>
+        <h2>Morning/Afternoon Work Strength</h2>
         <button class="button" @click="updatePieChart()">Update</button><br><br><br>
-        <pie-chart width =500px :data= "piechartdata"></pie-chart>
+        <pie-chart width =500px :data= "piechartdata" ></pie-chart>
     </div>
 </template>
 
@@ -29,6 +35,7 @@ export default {
 
         return{
             linechartdata: {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0, "Friday": 0},
+            linechartdata2: {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0, "Friday": 0},
             piechartdata: {"Morning": 0, "Afternoon": 0},
             selected:""
         }
@@ -83,6 +90,40 @@ export default {
 
                 console.log(data)
                 this.linechartdata = data  
+            }) 
+        },
+
+        
+        getWeek() {
+            var onejan = new Date(this.getFullYear(),0,1);
+            return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7);
+        },
+
+        updateLineChart2() {
+            var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+            var data = {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0, "Friday": 0};
+            
+            const shifts = db.collection("availabilities")
+
+            shifts.get().then((querySnapshot) => {
+                const temp = []
+                querySnapshot.forEach((doc) => {
+                    temp.push({ id: doc.id, ...doc.data() })
+
+                    if (doc.data().approved) {
+                        const today = new Date() 
+                        const date = doc.id.slice(0,10)
+                        if (new Date(date).getWeek() == new Date(today).getWeek()) {
+                            const day = new Date(date).getDay()
+                            const dayOfWeek = days[day]
+
+                            data[dayOfWeek] += 1
+                        }
+                    }
+                })
+ 
+                console.log(data)
+                this.linechartdata2 = data  
             }) 
         },
 
