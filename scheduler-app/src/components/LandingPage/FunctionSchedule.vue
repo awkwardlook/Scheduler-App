e726ea3ad4ce29c667a467c158c3f94089c8e3b6
vjo-anne209 <77625659+vjo-anne209@users.noreply.github.com>
2022-04-08 23:26:28 +0800

    Updated image css

--- scheduler-app/src/components/LandingPage/FunctionSchedule.vue
+++ scheduler-app/src/components/LandingPage/FunctionSchedule.vue
@@ -8,18 +8,14 @@
             <h4>Schedule Your Staff Easily</h4>
             <p style="font-size:16px;color:rgb(78, 66, 66);">Just by approving the shifts, a smart timesheet allocates shifts for you!</p>
             </div>
-            <div class="functionImage">
-                <img src="/img/calendar.png" style="width:250px;height:300px">
-            </div>
+            <img src="/img/calendar.png" class="func_img">
         </div>
          <div class="function-container">
-            <div class="functionImage">
-                <img src="/img/analytics.png">
-            </div>
+            <img src="/img/analytics.png" class="func_img">
             <div class="text">
                 <h2>Analytics</h2>
                 <h4>Work Peformance Report</h4>
-                <p style="font-size:16px;color:rgb(78, 66, 66);">
+                <p>
                 Keep track of how your stores and employee are doing. <br>
                 Have access to real time analytics.
                 </p>
@@ -40,18 +36,21 @@ export default {
     display: flex;
     align-items: center;
     justify-content: center;
-    
 }
 
-.functionImage {
-    max-width: 100%;
-    flex-basis: 70%;
+.func_img {
+    flex-basis: 20%;
 }
 
 .text {
     font-size: 20px;
     padding-left: 40px;
     text-align: justify;
+}
+
+.text > p {
+font-size: 16px; 
+color: rgb(78, 66, 66);
 }
 
 </style>
