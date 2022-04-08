9cb039b81b181c5d969b9085419de5ff3dfc8016
Edward Loke <awkwardlook@gmail.com>
2022-04-08 12:25:52 +0800

    Image Preview dimensions fixed

--- scheduler-app/src/components/LoginRegister/AddEmployee.vue
+++ scheduler-app/src/components/LoginRegister/AddEmployee.vue
@@ -33,14 +33,14 @@
       <v-flex  md6 offset-sm3 >
        <div>
          <div >
-           <v-btn @click="click1">Upload Profile Picture</v-btn>
+           <v-btn @click="click1">Click here to upload Profile Picture</v-btn>
            <input type="file" ref="input1"
-            style="display: none"
+            style="display: none" height="500px" width="200px"
             @change="previewImage" accept="image/*" >                
          </div>
  
        <div v-if="imageData!=null">                     
-          <img class="preview" height="268" width="356" :src="img1">
+          <img class="preview"  :src="img1">
        <br>
        </div>   
       
